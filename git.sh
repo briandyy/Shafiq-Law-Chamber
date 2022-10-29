125e165d7a5069404dabc91a2d82cc01a00eac8b
rafay99-epic <99marafay@gmail.com>
2022-10-30 01:57:20 +0500

    Creating Website

--- git.sh
+++ git.sh
@@ -1,7 +1,22 @@
 #!/bin/bash
-echo "Running Git Commands for SLC Project"
+echo "
+-------------------------------------
+Running Git Commands for SLC Project
+-------------------------------------"
+
 git status
 git add *
 git commit -m "Creating Website"
 git push
-echo "Command Completed!!"
+
+echo "
+-------------------
+Command Completed!!
+-------------------"
+
+echo "
+----------------------
+Running Node Js Server
+----------------------
+"
+exit 0
