b6972eb7b186f0f5935f1bb2a63782bffc273b3a
rafay99-epic <99marafay@gmail.com>
2022-10-31 13:32:23 +0500

    Updating Files in Development Branch

---
+++ git.ps1
@@ -0,0 +1,13 @@
+Write.Console("Working on SLC");
+
+Write.Console("Running git commands");
+
+git status
+git add * 
+git commit -m 'Updating Files in Development Branch'
+git pull
+
+Write.Console("Running Node JS Serve");
+npm start
+
+
