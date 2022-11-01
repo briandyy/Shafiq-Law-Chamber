a242817cdf24fabbe8ff1cea1c9aa927c75c17ce
rafay99-epic <99marafay@gmail.com>
2022-11-01 13:14:23 +0500

    Updating Files in Development Branch

--- git.ps1
+++ git.ps1
@@ -1,14 +1,33 @@
 Write-Host("Working on SLC");
+# Storing the console output into a Variable.
+$OutputVariable = (git branch --show-current) | Out-String
+# Displaying the Output from the variable
+# Get-Variable OutputVariable
 
-Write-Host("Running git commands");
+# Checking the Branch and then commiting on the bases of branch
+if( $OutputVariable -eq "main" ) {
+    Write-Host("On Main Branch");
+    # Checking the Current Status
+    git status
+    # Getting from the main remote repo
+    git pull 
+    
+} else 
+{
+    Write-Host("Not on master, it's the Development Branch");
+    Write-Host("Running git commands");
+    git status
+    git add * 
+    git commit -m 'Updating Files in Development Branch'
+    git push
+}
+# else {
+#     Write-Host("No Branch was Detected");
+#     Write-Host("Please try Again") 
+# }
 
-git status
-git add * 
-git commit -m 'Updating Files in Development Branch'
-git push
-
+# Running the NodeJs server.
 Write-Host("Running Node JS Serve");
-
 npm start
 
 
